915003a98a89f4f0cfac62b68a5e2e7158dc5633
Demis Bellot <demis.bellot@gmail.com>
2023-04-12 17:45:48 +0800

    implement initial Favorites

--- MyApp/wwwroot/mjs/components/Artifacts.mjs
+++ MyApp/wwwroot/mjs/components/Artifacts.mjs
@@ -4,6 +4,16 @@ import { queryString, ApiResult, combinePaths } from "@servicestack/client"
 import { Artifact, ArtifactResult, HardDeleteCreative, QueryArtifacts, QueryCreatives } from "../dtos.mjs"
 import { Store } from "../store.mjs"
 
+export const AlbumTitle = {
+    template:`<div>
+        <h3 class="text-xl">{{album.name}}</h3>
+    </div>`,
+    props:['album'],
+    setup(props) {
+        return {}
+    }
+}
+
 export const ArtifactImage = {
     template:`<div v-if="artifact" class="overflow-hidden" :style="store.getBackgroundStyle(artifact)">
       <img :alt="artifact.prompt" :width="width" :height="height" :class="imageClass"
@@ -57,37 +67,17 @@ export const ArtifactModal = {
                             <div class="w-full h-full absolute inset-0 z-10 block text-zinc-100 drop-shadow pointer-events-none line-clamp px-2 pb-2 text-sm opacity-0 group-hover:opacity-40 transition duration-300 ease-in-out bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black"></div>
             
                             <div class="absolute w-full h-full flex z-10 text-zinc-100 justify-between drop-shadow opacity-0 group-hover:opacity-100 transition-opacity mb-1 text-sm">
-                                <div class="relative w-full h-full overflow-hidden flex flex-col justify-between overflow-hidden">
-                                    <div class="p-4 flex justify-between flex-none">
-                                        <div class="cursor-pointer">
-                                            <a :href="store.searchBySimilarUrl(artifact.refId)">
-                                            <svg class="h-6 w-6 text-cyan-600 hover:text-cyan-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
-                                                <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5">
-                                                <title>explore similar</title>
-                                                <path d="m11.25 11.25l3 3"/><circle cx="7.5" cy="7.5" r="4.75"/></g>
-                                            </svg></a>
+                                <div class="p-4 relative w-full h-full overflow-hidden flex flex-col justify-between overflow-hidden">
+                                    <div class="flex flex-col h-full justify-between">
+                                        <div class="flex justify-between flex-none">
+                                            <ArtifactLikeIcon :artifact="artifact" @changed="$emit('changed')" />
+                                            <div class="px-1 cursor-pointer" @click.prevent.stop="showArtifactMenu($event, artifact, 140)">
+                                                <svg class="w-5 h-5 text-cyan-600 hover:text-cyan-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+                                                    <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><circle cx="8" cy="2.5" r=".75" /><circle cx="8" cy="8" r=".75" /><circle cx="8" cy="13.5" r=".75" /></g>
+                                                </svg>
+                                            </div>
                                         </div>
-                                        <div class="px-1 cursor-pointer" @click.prevent.stop="showArtifactMenu($event, artifact, 140)">
-                                            <svg class="w-5 h-5 text-cyan-600 hover:text-cyan-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
-                                                <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><circle cx="8" cy="2.5" r=".75" /><circle cx="8" cy="8" r=".75" /><circle cx="8" cy="13.5" r=".75" /></g>
-                                            </svg>
-                                        </div>
-                                    </div>
-            
-                                    <div>
-                                        <div class="pb-2 px-4">
-                                            <svg v-if="store.hasLiked(artifact)" @click.prevent.stop="unlikeArtifact(artifact)"
-                                                class="cursor-pointer mr-4 w-6 h-6 text-red-600 hover:text-red-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                                <title>undo like</title>
-                                                <path fill="currentColor" d="M2 8.4A5.4 5.4 0 0 1 7.5 3A5.991 5.991 0 0 1 12 5a5.991 5.991 0 0 1 4.5-2A5.4 5.4 0 0 1 22 8.4c0 5.356-6.379 9.4-10 12.6C8.387 17.773 2 13.76 2 8.4Z" />
-                                            </svg>
-                                            <svg v-else @click.prevent.stop="likeArtifact(artifact)"
-                                                class="cursor-pointer mr-4 w-6 h-6 text-cyan-600 hover:text-cyan-400" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
-                                                <title>like</title>
-                                                <path fill="currentColor" d="M12 21c-.645-.572-1.374-1.167-2.145-1.8h-.01c-2.715-2.22-5.792-4.732-7.151-7.742c-.446-.958-.683-2-.694-3.058A5.39 5.39 0 0 1 7.5 3a6.158 6.158 0 0 1 3.328.983A5.6 5.6 0 0 1 12 5c.344-.39.738-.732 1.173-1.017A6.152 6.152 0 0 1 16.5 3A5.39 5.39 0 0 1 22 8.4a7.422 7.422 0 0 1-.694 3.063c-1.359 3.01-4.435 5.521-7.15 7.737l-.01.008c-.772.629-1.5 1.224-2.145 1.8L12 21ZM7.5 5a3.535 3.535 0 0 0-2.5.992A3.342 3.342 0 0 0 4 8.4c.011.77.186 1.53.512 2.228A12.316 12.316 0 0 0 7.069 14.1c.991 1 2.131 1.968 3.117 2.782c.273.225.551.452.829.679l.175.143c.267.218.543.444.81.666l.013-.012l.006-.005h.006l.009-.007h.01l.018-.015l.041-.033l.007-.006l.011-.008h.006l.009-.008l.664-.545l.174-.143c.281-.229.559-.456.832-.681c.986-.814 2.127-1.781 3.118-2.786a12.298 12.298 0 0 0 2.557-3.471c.332-.704.51-1.472.52-2.25A3.343 3.343 0 0 0 19 6a3.535 3.535 0 0 0-2.5-1a3.988 3.988 0 0 0-2.99 1.311L12 8.051l-1.51-1.74A3.988 3.988 0 0 0 7.5 5Z" />
-                                            </svg>
-                                        </div>
-                                        <div class="mt-4"></div>
+                                        <ArtifactExploreIcon class="pb-2" :artifact="artifact" v-on:click.stop />
                                     </div>
                                 </div>
                             </div>
@@ -171,8 +161,8 @@ export const ArtifactModal = {
             </slot>
         </div>
     </ModalDialog>`,
-    emits:['done','selected'],
-    props:['artifact'],
+    emits:['done','selected','changed'],
+    props:['artifact', 'resolve-border-color'],
     setup(props, { emit }) {
         const client = useClient()
         const { bytes } = useFormatters()
@@ -193,9 +183,11 @@ export const ArtifactModal = {
         /** @param {Artifact} artifact
          *  @param {number} [selectedId] */
         function resolveBorderColor(artifact, selectedId) {
-            return selectedId && artifact.id == selectedId
-                ? 'border-yellow-300'
-                : store.resolveBorderColor(artifact)
+            return props.resolveBorderColor 
+                ? props.resolveBorderColor(artifact, selectedId)
+                : selectedId && artifact.id == selectedId
+                    ? 'border-yellow-300'
+                    : store.resolveBorderColor(artifact)
         }
 
         function navNext(next) {
@@ -238,6 +230,63 @@ export const ArtifactModal = {
     }
 }
 
+export const ArtifactLikeIcon = {
+    template:`<svg v-if="store.hasLiked(artifact)" @click.prevent.stop="unlikeArtifact(artifact)"
+         class="cursor-pointer w-4 h-4 sm:w-6 sm:h-6 text-red-600 hover:text-red-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <title>undo like</title>
+        <path fill="currentColor" d="M2 8.4A5.4 5.4 0 0 1 7.5 3A5.991 5.991 0 0 1 12 5a5.991 5.991 0 0 1 4.5-2A5.4 5.4 0 0 1 22 8.4c0 5.356-6.379 9.4-10 12.6C8.387 17.773 2 13.76 2 8.4Z"/>
+    </svg>
+    <svg v-else @click.prevent.stop="likeArtifact(artifact)"
+         class="cursor-pointer w-4 h-4 sm:w-6 sm:h-6 text-cyan-600 hover:text-cyan-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <title>like</title>
+        <path fill="currentColor" d="M12 21c-.645-.572-1.374-1.167-2.145-1.8h-.01c-2.715-2.22-5.792-4.732-7.151-7.742c-.446-.958-.683-2-.694-3.058A5.39 5.39 0 0 1 7.5 3a6.158 6.158 0 0 1 3.328.983A5.6 5.6 0 0 1 12 5c.344-.39.738-.732 1.173-1.017A6.152 6.152 0 0 1 16.5 3A5.39 5.39 0 0 1 22 8.4a7.422 7.422 0 0 1-.694 3.063c-1.359 3.01-4.435 5.521-7.15 7.737l-.01.008c-.772.629-1.5 1.224-2.145 1.8L12 21ZM7.5 5a3.535 3.535 0 0 0-2.5.992A3.342 3.342 0 0 0 4 8.4c.011.77.186 1.53.512 2.228A12.316 12.316 0 0 0 7.069 14.1c.991 1 2.131 1.968 3.117 2.782c.273.225.551.452.829.679l.175.143c.267.218.543.444.81.666l.013-.012l.006-.005h.006l.009-.007h.01l.018-.015l.041-.033l.007-.006l.011-.008h.006l.009-.008l.664-.545l.174-.143c.281-.229.559-.456.832-.681c.986-.814 2.127-1.781 3.118-2.786a12.298 12.298 0 0 0 2.557-3.471c.332-.704.51-1.472.52-2.25A3.343 3.343 0 0 0 19 6a3.535 3.535 0 0 0-2.5-1a3.988 3.988 0 0 0-2.99 1.311L12 8.051l-1.51-1.74A3.988 3.988 0 0 0 7.5 5Z"/>
+    </svg>`,
+    emits:['showAuth','changed'],
+    props:['artifact'],
+    setup(props, { emit }) {
+        /** @type {Store} */
+        const store = inject('store')
+        const { user, hasRole } = useAuth()
+        const instance = getCurrentInstance()
+
+        /** @param {Artifact} artifact */
+        async function unlikeArtifact(artifact) {
+            if (!user.value) {
+                emit('showAuth')
+                return
+            }
+            await store.unlikeArtifact(artifact.id)
+            emit('changed')
+            instance?.proxy?.$forceUpdate()
+        }
+        /** @param {Artifact} artifact */
+        async function likeArtifact(artifact) {
+            if (!user.value) {
+                emit('showAuth')
+                return
+            }
+            await store.likeArtifact(artifact.id)
+            emit('changed')
+            instance?.proxy?.$forceUpdate()
+        }
+
+        return { store, unlikeArtifact, likeArtifact }
+    }
+}
+export const ArtifactExploreIcon = {
+    template:`<a class="block" :href="'/?similar=' + artifact.refId">
+        <svg class="w-4 h-4 sm:w-6 sm:h-6 text-cyan-600 hover:text-cyan-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+            <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5">
+                <title>explore similar</title>
+                <path d="m11.25 11.25l3 3" /><circle cx="7.5" cy="7.5" r="4.75" />
+            </g>
+        </svg>
+    </a>`,
+    props:['artifact'],
+    setup(props) {
+    }
+}
+
 export const ArtifactGallery = {
     template:`<div>
         <div :class="['grid',store.css.gridClass(store.prefs.artifactGalleryColumns)]">
@@ -253,18 +302,10 @@ export const ArtifactGallery = {
     
                             <div class="absolute w-full h-full flex z-10 text-zinc-100 justify-between drop-shadow opacity-0 group-hover:opacity-100 transition-opacity sm:mb-1 text-sm">
                                 <div class="relative w-full h-full overflow-hidden flex flex-col justify-between overflow-hidden">
-                                    <div class="sm:p-3">
-                                        <svg v-if="store.hasLiked(artifact)" @click.prevent.stop="unlikeArtifact(artifact)"
-                                             class="w-4 h-4 sm:w-6 sm:h-6 text-red-600 hover:text-red-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                            <title>undo like</title>
-                                            <path fill="currentColor" d="M2 8.4A5.4 5.4 0 0 1 7.5 3A5.991 5.991 0 0 1 12 5a5.991 5.991 0 0 1 4.5-2A5.4 5.4 0 0 1 22 8.4c0 5.356-6.379 9.4-10 12.6C8.387 17.773 2 13.76 2 8.4Z"/>
-                                        </svg>
-                                        <svg v-else @click.prevent.stop="likeArtifact(artifact)"
-                                             class="w-4 h-4 sm:w-6 sm:h-6 text-cyan-600 hover:text-cyan-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                            <title>like</title>
-                                            <path fill="currentColor" d="M12 21c-.645-.572-1.374-1.167-2.145-1.8h-.01c-2.715-2.22-5.792-4.732-7.151-7.742c-.446-.958-.683-2-.694-3.058A5.39 5.39 0 0 1 7.5 3a6.158 6.158 0 0 1 3.328.983A5.6 5.6 0 0 1 12 5c.344-.39.738-.732 1.173-1.017A6.152 6.152 0 0 1 16.5 3A5.39 5.39 0 0 1 22 8.4a7.422 7.422 0 0 1-.694 3.063c-1.359 3.01-4.435 5.521-7.15 7.737l-.01.008c-.772.629-1.5 1.224-2.145 1.8L12 21ZM7.5 5a3.535 3.535 0 0 0-2.5.992A3.342 3.342 0 0 0 4 8.4c.011.77.186 1.53.512 2.228A12.316 12.316 0 0 0 7.069 14.1c.991 1 2.131 1.968 3.117 2.782c.273.225.551.452.829.679l.175.143c.267.218.543.444.81.666l.013-.012l.006-.005h.006l.009-.007h.01l.018-.015l.041-.033l.007-.006l.011-.008h.006l.009-.008l.664-.545l.174-.143c.281-.229.559-.456.832-.681c.986-.814 2.127-1.781 3.118-2.786a12.298 12.298 0 0 0 2.557-3.471c.332-.704.51-1.472.52-2.25A3.343 3.343 0 0 0 19 6a3.535 3.535 0 0 0-2.5-1a3.988 3.988 0 0 0-2.99 1.311L12 8.051l-1.51-1.74A3.988 3.988 0 0 0 7.5 5Z"/>
-                                        </svg>
-                                    </div>
+
+                                    <slot name="icons" v-bind="artifact">
+                                        <div></div>
+                                    </slot>
     
                                     <div v-if="store.isArtifactResult(artifact)" class="hidden sm:flex bg-black/40 sm:pt-2 sm:pl-4 sm:pb-4 sm:pr-4 w-full">
                                         <div class="w-full">
@@ -287,12 +328,13 @@ export const ArtifactGallery = {
                 </div>
             </div>
         </div>
-        <ArtifactModal v-if="creative && active" :artifact="active" @selected="navTo" @done="modalClose" />
+        <ArtifactModal v-if="creative && active" :artifact="active" @selected="navTo" @done="modalClose" @changed="$emit('changed')" />
         <SignInDialog v-if="showAuth && !showSignUp" @done="showAuth=false" @signup="showSignUp=true" />
         <SignUpDialog v-if="showAuth && showSignUp" @done="showAuth=false"  @signin="showSignUp=false" />
     </div>`,
+    emits:['changed'],
     props:['results'],
-    setup(props) {
+    setup(props, { expose }) {
         const client = useClient()
         /** @type {Store} */
         const store = inject('store')
@@ -309,6 +351,12 @@ export const ArtifactGallery = {
         const apiCreative = ref(new ApiResult())
         const creative = computed(() => apiCreative.value.response?.results?.[0])
         
+        function forceUpdate() {
+            console.log('ArtifactGallery.forceUpdate()')
+            instance?.proxy?.$forceUpdate()
+        }
+        expose({ forceUpdate })
+        
         /** @param {Artifact} artifact */
         function navTo(artifact) {
             const args = artifact ? { view: artifact.id } : undefined
@@ -319,7 +367,7 @@ export const ArtifactGallery = {
         function modalClose() {
             selected.value = null
             viewing.value = null
-            pushState({ id:undefined, artifactId:undefined })
+            pushState({ view:undefined, artifactId:undefined })
         }
         
         /** @param {Artifact} artifact
@@ -329,25 +377,7 @@ export const ArtifactGallery = {
                 ? 'border-yellow-300'
                 : store.resolveBorderColor(artifact)
         }
-        
-        /** @param {Artifact} artifact */
-        async function unlikeArtifact(artifact) {
-            if (!user.value) {
-                showAuth.value = true
-                return
-            }
-            await store.unlikeArtifact(artifact.id)
-            instance?.proxy?.$forceUpdate()
-        }
-        /** @param {Artifact} artifact */
-        async function likeArtifact(artifact) {
-            if (!user.value) {
-                showAuth.value = true
-                return
-            }
-            await store.likeArtifact(artifact.id)
-            instance?.proxy?.$forceUpdate()
-        }
+
         /** @param {MouseEvent} e
          *  @param {Artifact} artifact */
         function showArtifactMenu(e, artifact) {
@@ -374,8 +404,7 @@ export const ArtifactGallery = {
 
         return { 
             store, selected, creative, viewing, active,
-            navTo, modalClose, likeArtifact, unlikeArtifact,
-            resolveBorderColor, showArtifactMenu,
+            navTo, modalClose, resolveBorderColor, showArtifactMenu,
             showAuth, showSignUp,
         }
     }
